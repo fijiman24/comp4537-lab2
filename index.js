b328cfd902c7ccc0b49f751912d2487d216ad2e9
CALEB VERMA <halasmish@gmail.com>
2023-01-14 16:49:26 -0800

    fix port settings

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3030;
 
 app.use(express.json()); // read JSON BODY
 app.use(express.urlencoded({ extended: true })); // read URL encoded bdoy
 
-// app.listen(PORT, () => {
-//     console.log(`Server is running on port ${PORT}`);
-// });
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
 app.post('/chatbot', (req, res) => {
     // Handle incoming request
