a803784b43bff3e9b8d67635ef97a4c4b7f2a06d
CALEB VERMA <halasmish@gmail.com>
2023-01-14 16:41:45 -0800

    move jquery stuff to separate js file

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
-const { window } = new JSDOM();
-const { document } = (new JSDOM('')).window;
-global.document = document;
-
-var $ = jQuery = require('jquery')(window);
 
 app.use(express.json()); // read JSON BODY
 app.use(express.urlencoded({ extended: true })); // read URL encoded bdoy
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
 
 app.post('/chatbot', (req, res) => {
     // Handle incoming request
@@ -37,32 +30,4 @@ app.post('/chatbot', (req, res) => {
             text: "I'm sorry, I didn't understand your question. Please provide a number for me to give you information about."
         });
     }
-});
-
-$(document).ready(function () {
-    $('#chatbot-form').submit(function (event) {
-        event.preventDefault();
-        const message = $('#chatbot-input').val();
-        $.ajax({
-            type: 'POST',
-            url: 'http://localhost:3000/chatbot',
-            data: {
-                message: message
-            },
-            success: function (response) {
-                let newMessage = $('<div>', {
-                    class: 'message'
-                }).text(response.text);
-                let removeButton = $('<button>', {
-                    class: 'remove-button'
-                }).text('Remove');
-                newMessage.append(removeButton);
-                $('#chat-history').append(newMessage);
-            }
-        });
-    });
-});
-
-$(document).on('click', '.remove-button', function() {
-	$(this).parent().remove();
 });
